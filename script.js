90c29f53f21b775e936136d21bb57d6d7571320d
chtrubey <59847182+chtrubey@users.noreply.github.com>
2020-01-15 16:03:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var nameEle = document.body.querySelector(".name");
+var sumEle = document.body.querySelector(".sum");
+
+var name = prompt("What is your name?");
+var age = Number(prompt("What is your age?"));
+var number = Number(prompt("What is your favorite number?"));
+var sum = number+age;
+
+nameEle.innerHTML=name+" is my name."
+sumEle.innerHTML= "Them sum of my age and favorite number is "+sum;
